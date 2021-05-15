4c3bb86f33940b1c5773254a7b5ac5b6462b96b2
Mashxurbek_Muhammadjonov <mashxurbekmuhammadjonov@gmail.com>
2021-05-15 15:06:21 +0500

    pets-section_completed

--- src/js/main.js
+++ src/js/main.js
@@ -3,11 +3,13 @@ var burgerSpan = document.querySelector(".burger__span");
 var headerInner = document.querySelector(".main-header__inner");
 var searchBtn = document.querySelector(".main-header__search-btn");
 var search = document.querySelector(".main-header__search");
+var body = document.querySelector(".body");
 
 burger.addEventListener("click", function() {
   burgerSpan.classList.toggle("burger__span--active")
   burger.classList.toggle("burger--active")
   headerInner.classList.toggle("main-header__inner--active")
+  body.classList.toggle("body--active")
 });
 
 searchBtn.addEventListener("click", function() {
